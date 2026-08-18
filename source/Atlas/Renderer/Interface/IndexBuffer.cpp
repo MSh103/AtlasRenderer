75fe19e4e6e7694388cca9e02823e15234978322
Mohammad Shaikh <mohammad@oshaikh.sa>
2026-08-19 01:52:52 +0300

    Implement Logging

--- source/Atlas/Renderer/Interface/IndexBuffer.cpp
+++ source/Atlas/Renderer/Interface/IndexBuffer.cpp
@@ -2,6 +2,8 @@
 
 #include "Atlas/Renderer/Core/OpenGLEssentials.h"
 
+#include <Atlas/Core/Log.h>
+
 namespace Atlas
 {
 
@@ -11,11 +13,13 @@ namespace Atlas
 		GLCall(glCreateBuffers(1, &m_RendererID));
 		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID));
 		GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(uint32_t), indices, GL_STATIC_DRAW));
+		Atlas::Log::Core::Trace("Generated IndexBuffer: {}", m_RendererID);
 	}
 
 	IndexBuffer::~IndexBuffer()
 	{
 		GLCall(glDeleteBuffers(1, &m_RendererID));
+		Atlas::Log::Core::Trace("Deleted IndexBuffer: {}", m_RendererID);
 	}
 
 	void IndexBuffer::Use()
